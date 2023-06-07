7128d3bdef56ba2cb8d7595d47d5385d10da5a81
arjunn881 <arjunn881@gmail.com>
2023-06-07 20:40:02 +0530

    07/06 20:40

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.js";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://arjunn881:admin@cluster0.kphrut6.mongodb.net/?retryWrites=true&w=majority"
-).then(()=>{
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
     console.log("Connected to Database");
-}).catch(((error)=>{
+  })
+  .catch((error) => {
     console.log(error);
-}));
-app.listen(1000, () => {
+  });
+
+app.get("/api/test", (req, res) => {
+  res.status(200).json("Ecommerce API !!!");
+});
+
+app.use("/api/user", userRouter);
+app.listen(process.env.PORT || 1000, () => {
   console.log("Backend server is running.......");
 });
