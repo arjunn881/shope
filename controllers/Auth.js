8cbced8f85dc9d931aea0f1a6db68c815452ef5c
arjunn881 <arjunn881@gmail.com>
2023-06-11 21:36:25 +0530

    11/06 21:36

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,35 +1,51 @@
 import User from "../models/user.js";
 import CryptoJS from "crypto-js";
+import Jwt from "jsonwebtoken";
 
-export const register = async(req,res)=>{
-    const  newUser = new User({
-        username:req.body.username,
-        email:req.body.email,
-        password:req.body.password
-       
-    })
- try {
+export const register = async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_sec
+    ).toString(),
+  });
+  try {
     const savedUser = await newUser.save();
     res.status(200).json(savedUser);
- } catch (error) {
-    res.status(500).json(error)
- }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-}
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong credentials!!!");
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_sec
+    );
 
-export const login = async(req,res)=>{
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-try {
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials!!!");
+    const { password, ...others } = user._doc;
 
+    const acessToken = Jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "1d" }
+    );
 
-   const user = await User.findOne({email:req.body.email});
-   !user && res.status(401).json("Wrong credentials!!!");
-   user && res.status(200).json(user);
-   res.status(200).json(savedUser);
-} catch (error) {
-   res.status(500).json(error)
-}
-
-}
-
-
+    others && res.status(200).json({...others, acessToken});
+    res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
