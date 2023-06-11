60a78769a578470cfa3d70af281bda257df78186
arjunn881 <arjunn881@gmail.com>
2023-06-11 20:59:18 +0530

    11/06 20:59

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -5,7 +5,7 @@ export const register = async(req,res)=>{
     const  newUser = new User({
         username:req.body.username,
         email:req.body.email,
-        password:CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+        password:req.body.password
        
     })
  try {
@@ -16,3 +16,20 @@ export const register = async(req,res)=>{
  }
 
 }
+
+export const login = async(req,res)=>{
+
+try {
+
+
+   const user = await User.findOne({email:req.body.email});
+   !user && res.status(401).json("Wrong credentials!!!");
+   user && res.status(200).json(user);
+   res.status(200).json(savedUser);
+} catch (error) {
+   res.status(500).json(error)
+}
+
+}
+
+
