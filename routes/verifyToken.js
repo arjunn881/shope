2ca32ccb3b5f4a6368eac8f6b1ca2f40377d24b2
arjunn881 <arjunn881@gmail.com>
2023-06-15 20:40:21 +0530

    15/06 20:40

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -5,13 +5,14 @@ const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
 
   if (authHeader) {
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
       if (err) return req.status(401).json("Token is not valid");
       req.user = user;
       next();
     });
   } else {
-    return req.status(401).json("You are not authenticated");
+    return res.status(401).json("You are not authenticated");
   }
 };
 
